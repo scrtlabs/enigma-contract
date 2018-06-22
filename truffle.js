26de6ccffb5d91e7c1d7406957abea150f123843
Frederic Fortier <fredfortier@gmail.com>
2018-06-22 13:03:57 -0700

    BLD: upgrading to ES6 and Truffle.next

--- truffle.js
+++ truffle.js
@@ -1,14 +1,28 @@
+require('babel-register')({
+  ignore: /node_modules\/(?!openzeppelin-solidity\/test\/helpers)/
+})
+require('babel-polyfill')
+
+// See <http://truffleframework.com/docs/advanced/configuration>
+// to customize your Truffle configuration!
 module.exports = {
-    networks: {
-        development: {
-            host: "127.0.0.1",
-            port: 9545,
-            network_id: "*"
-        },
-        ganache: {
-            host: "127.0.0.1",
-            port: 8545,
-            network_id: "*" 
-        }
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 9545,
+      network_id: '*' // Match any network id
     }
-};
+  },
+  solc: {
+    // Turns on the Solidity optimizer. For development the optimizer's
+    // quite helpful, just remember to be careful, and potentially turn it
+    // off, for live deployment and/or audit time. For more information,
+    // see the Truffle 4.0.0 release notes.
+    //
+    // https://github.com/trufflesuite/truffle/releases/tag/v4.0.0
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  }
+}
