88d694f2231889df8ba6af96a20511b23a1d35f8
Victor Grau Serrat <lacabra@users.noreply.github.com>
2018-06-22 16:54:10 -0600

    MAINT: adjusting travis to darq-truffle@next

--- truffle.js
+++ truffle.js
@@ -17,6 +17,7 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" 
+    }
   },
   solc: {
     // Turns on the Solidity optimizer. For development the optimizer's
