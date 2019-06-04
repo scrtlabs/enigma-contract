5cd45b8e63011030c6fdf43a8fa766c064a55b3e
Victor Grau Serrat <lacabra@users.noreply.github.com>
2019-06-04 17:06:34 -0600

    BLD: webpack bundle for Node.js

--- enigma-js/src/index.js
+++ enigma-js/src/index.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill';
-window.Promise = Promise;
+// window.Promise = Promise;
 import utils from './enigma-utils';
 import Enigma from './Enigma';
+import * as eeConstants from './emitterConstants';
 
-export {Enigma, utils};
+export {Enigma, utils, eeConstants};
--- enigma-js/webpack.config.js
+++ enigma-js/webpack.config.js
@@ -1,9 +1,12 @@
 /* global __dirname, require, module*/
 
-const webpack = require('webpack');
 const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const CreateFileWebpack = require('create-file-webpack')
 const env = require('yargs').argv.env; // use --env with webpack 2
 const pkg = require('./package.json');
+
 
 let libraryName = pkg.name;
 
@@ -12,12 +15,15 @@ let outputFile, mode;
 if (env === 'build') {
   mode = 'production';
   outputFile = libraryName + '.min.js';
+  outputFileNode = libraryName + '.node.min.js';
 } else {
   mode = 'development';
   outputFile = libraryName + '.js';
+  outputFileNode = libraryName + '.node.js';
 }
 
 const config = {
+  target: 'web',
   mode: mode,
   entry: [__dirname + '/src/index.js'],
   devtool: 'source-map',
@@ -51,4 +57,40 @@ const config = {
   },
 };
 
-module.exports = config;
+const serverConfig = {
+  target: 'async-node',
+  mode: mode,
+  entry:  [__dirname + '/src/index.js'],
+  devtool: 'source-map',
+  plugins: [
+    new CreateFileWebpack({
+      path: __dirname,
+      fileName: 'node.js',
+      content: `const {Enigma, utils, eeConstants} = require('./lib/${outputFileNode}');\nmodule.exports = {Enigma, utils, eeConstants};`,
+    })
+  ],
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFileNode,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: [nodeExternals()],
+}
+
+module.exports = [config, serverConfig];
