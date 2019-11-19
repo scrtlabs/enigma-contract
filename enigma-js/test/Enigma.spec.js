be0451d8f52c32d529609f49790ac5b658b50e1a
Aditya Palepu <apalepu23@gmail.com>
2019-11-18 23:46:37 -0800

    DEV: fixing unit test

--- enigma-js/test/Enigma.spec.js
+++ enigma-js/test/Enigma.spec.js
@@ -2442,24 +2442,24 @@ describe('Enigma tests', () => {
         ' contract');
     });
 
-    // it('should fail to re-register a worker with new contract due to an invalid signature', async () => {
-    //   const workerParams = await enigma.getWorkerParams(latestTask.creationBlockNumber);
-    //   const selectedWorkerAddr = (await enigma.selectWorkerGroup(latestTask.scAddr, workerParams, 1))[0];
-    //   let worker = await enigma.admin.findBySigningAddress(selectedWorkerAddr);
-    //   const index = accounts.indexOf(worker.account);
-    //   const workerData = data.workers[index];
-    //   const report = '0x' + Array.from(workerData[1]).map((c) => c.charCodeAt(0).toString(16)).join('');
-    //   const signature = '0x' + workerData[3];
-    //   const proof = utils.hash(['0x00']);
-    //   const sig = await enigma.web3.eth.sign(proof, worker.account);
-    //   await expect(new Promise((resolve, reject) => {
-    //     enigmaUpgradedContract.methods.register(workerData[0], report, signature, sig).send({
-    //       gas: 4712388,
-    //       gasPrice: 100000000000,
-    //       from: worker.account,
-    //     }).on('receipt', (receipt) => resolve(receipt)).on('error', (error) => reject(error.message));
-    //   })).rejects.toEqual('Returned error: VM Exception while processing transaction: revert Invalid signature');
-    // });
+    it('should fail to re-register a worker with new contract due to an invalid signature', async () => {
+      const workerParams = await enigma.getWorkerParams(latestTask.creationBlockNumber);
+      const selectedWorkerAddr = (await enigma.selectWorkerGroup(latestTask.scAddr, workerParams, 1))[0];
+      let worker = await enigma.admin.findBySigningAddress(selectedWorkerAddr);
+      const index = accounts.indexOf(worker.account);
+      const workerData = data.workers[index];
+      const report = '0x' + Array.from(workerData[1]).map((c) => c.charCodeAt(0).toString(16)).join('');
+      const signature = '0x' + workerData[3];
+      const proof = utils.hash(['0x00']);
+      const sig = await enigma.web3.eth.sign(proof, worker.account);
+      await expect(new Promise((resolve, reject) => {
+        enigmaUpgradedContract.methods.register(workerData[0], report, signature, sig).send({
+          gas: 4712388,
+          gasPrice: 100000000000,
+          from: worker.account,
+        }).on('receipt', (receipt) => resolve(receipt)).on('error', (error) => reject(error.message));
+      })).rejects.toEqual('Returned error: VM Exception while processing transaction: revert Invalid signature');
+    });
 
     it('should re-register workers and confirm worker balance has been transferred', async () => {
       const workerParams = await enigma.getWorkerParams(latestTask.creationBlockNumber);
